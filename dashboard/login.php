6f5dd0283ec719dac94db9d40a4dee46c33a1f4b
multiventures <dailystar.co.ke@gmail.com>
2024-05-07 10:51:34 +0300

    "Repair login for selecting first details only"

--- dashboard/login.php
+++ dashboard/login.php
@@ -2,26 +2,36 @@
     session_start();
     $pass = $_POST['adPass'];
     $uname = $_POST['adName'];
-    $con = mysqli_connect('localhost','biocharc_admin_init','Bcr<>23@Ng&F','biocharc_YmlvY2hhcmRib25l');
-    // $con = mysqli_connect('localhost','root','','biochar');
+    $conn = mysqli_connect('localhost','biocharc_admin_init','Bcr<>23@Ng&F','biocharc_YmlvY2hhcmRib25l');
+    // $conn = mysqli_connect('localhost','root','','biochar');
     if (mysqli_connect_errno()) {
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
         exit();
     }
-    $sql = "select adminPassword, adminName from admindetails";
+    $sql = "SELECT adminPassword, adminName FROM admindetails WHERE adminName = '$uname'";
+    $result = $conn->query($sql);
 
-    $result = mysqli_query($con, $sql);
-    while($row = mysqli_fetch_assoc($result)) {
-        $userName = $row['adminName'];
-        $paswd = $row['adminPassword'];
+// Check if the query executed successfully
+if ($result === false) {
+    echo "Error executing query: " . $conn->error;
+} else {
+    $result = mysqli_query($conn, $sql);
+    if ($result->num_rows > 0) {
+        // Fetch data from the result set
+        while ($row = $result->fetch_assoc()) {
+            $paswd = $row['adminPassword'];
+            $userName = $row['adminName'];
+        }
+        if($userName == $uname && $paswd == $pass){
+            echo "1";
+            $_SESSION['uname'] = $userName;
+        }
+        else{
+            echo("Wrong name or password, please try again");
+        }
+    } else {
+        echo "No results found for admin name: " . $uname;
     }
-    if($userName == $uname && $paswd == $pass){
-        echo "1";
-        $_SESSION['uname'] = $userName;
-    }
-    else{
-        echo($userName.$uname);
-        echo($paswd.$pass);
-        echo("Wrong name or password, please try again");
-    }
+}
+$conn->close();
 ?>
