4a532323133e2ddfbf1d0c80f3ba82989fc4bce7
multiventures <dailystar.co.ke@gmail.com>
2024-05-08 21:31:46 +0300

    Create sendmail.php

---
+++ dashboard/sendmail.php
@@ -0,0 +1,23 @@
+<?php
+
+    if($_POST['from'] == "" || $_POST['receiver'] == "" || $_POST['content'] == "" || $_POST['subject'] == ""){
+        echo"Please enter all the required values";
+    }
+    else{
+        $to = $_POST['receiver'];
+        $subject = $_POST['subject'];
+        $message = $_POST['content'];
+        $from = $_POST['from'];
+    }
+    $headers = array(
+        "MIME-Version" => "1.0",
+        "Content-Type" => "text/html;charset=UTF-8",
+        "From" => $from,
+        "Reply-To" => $from
+    );
+
+    // $message = file_get_contents("mail-template.html");
+
+    $send = mail($to, $subject, $message, $headers);
+
+    echo($send ? "Mail is send successfully" : "There was an error" );
