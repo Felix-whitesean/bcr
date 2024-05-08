2027d94b533f233326909e6f9f6d736c4e074c42
multiventures <dailystar.co.ke@gmail.com>
2024-05-08 17:13:22 +0300

    repair team social and db

--- revamp/team/index.php
+++ revamp/team/index.php
@@ -59,13 +59,13 @@
                 <div class="usercontent">
                     <ol>
                         <?php
-                            // $dsn = 'mysql:host=localhost;dbname=biocharc_YmlvY2hhcmRib25l';
-                            // $username = 'biocharc_admin_init';
-                            // $password = 'Bcr<>23@Ng&F'; 
+                            $dsn = 'mysql:host=localhost;dbname=biocharc_YmlvY2hhcmRib25l';
+                            $username = 'biocharc_admin_init';
+                            $password = 'Bcr<>23@Ng&F'; 
                             
-                            $dsn = 'mysql:host=localhost;dbname=biochar';
-                            $username = 'root';
-                            $password = ''; 
+                            // $dsn = 'mysql:host=localhost;dbname=biochar';
+                            // $username = 'root';
+                            // $password = ''; 
                             try {
                                 $pdo = new PDO($dsn, $username, $password);
                                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
