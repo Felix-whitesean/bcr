3b7c1ef8089717b2c7a93a2647046c0c40e1ab85
multiventures <dailystar.co.ke@gmail.com>
2024-05-20 23:22:45 +0300

    Update sendmail.php

--- dashboard/sendmail.php
+++ dashboard/sendmail.php
@@ -1,19 +1,18 @@
 <?php
-    if($_POST['receiver'] == "" || $_POST['content'] == "" || $_POST['subj'] == ""){
-        // echo"Please enter all the required values";
-        echo $_POST['from']." ".$_POST['receiver']." ".$_POST['content']." ".$_POST['subj'];
+    $from_email = $_POST['from'];
+    $to_email = $_POST['receiver'];
+    $subject = $_POST['subj'];
+    $content = $_POST['content'];
+    if($to_email == "" || $content == "" || $subject == ""){
+        echo"Please enter all the required values";
+        // echo $_POST['from']." ".$_POST['receiver']." ".$_POST['content']." ".$_POST['subj'];
     }
     else{
         // $to = $_POST['receiver'];
         // $subject = $_POST['subj'];
         // $message = $_POST['content'];
         // $from = $_POST['from'];
-
-        // Get form data
-        $from_email = $_POST['from'];
-        $to_email = $_POST['receiver'];
-        $subject = $_POST['subj'];
-        $content = $_POST['content'];
+          
         // Set headers
         $headers = "From: $from_email\r\n";
         $headers .= "Reply-To: $from_email\r\n";
